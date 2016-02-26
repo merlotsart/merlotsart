4896107637e443620da213c36616a412d6ec6a7e
DAVID MOORE <khundawg@gmail.com>
2016-02-26 16:29:22 -0500

    Add validations to LicenseeApplication
    Adding basic presence and format validations.

--- app/models/licensee_application.rb
+++ app/models/licensee_application.rb
@@ -1,2 +1,14 @@
 class LicenseeApplication < ActiveRecord::Base
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true
+  validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i }
+  validates :phone, presence: true
+  validates :interested_locations, presence: true
+
+  def full_name
+    first_name + last_name
+  end
+
 end
