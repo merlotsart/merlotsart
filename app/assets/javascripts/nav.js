1d7457fc53b2dbfbb1e4ddd63cd8886695bd7210
DAVID MOORE <khundawg@gmail.com>
2016-03-15 17:44:21 -0400

    Update site logo
    This commit adds a flip between white and color logos in the nav based
    on the scrolled position.

--- app/assets/javascripts/nav.js
+++ app/assets/javascripts/nav.js
@@ -2,9 +2,13 @@ $(window).scroll(function(){
   if ($(".navbar").offset().top > 20) {
     $(".navbar-fixed-top").addClass("top-nav-scroll");
     $(".navbar-fixed-top").removeClass("top-nav-scroll-reverse");
+    $(".merlots-logo-white").css('display', 'none');
+    $(".merlots-logo-color").css('display', 'block');
   } else {
     $(".navbar-fixed-top").removeClass("top-nav-scroll");
     $(".navbar-fixed-top").addClass("top-nav-scroll-reverse");
+    $(".merlots-logo-white").css('display', 'block');
+    $(".merlots-logo-color").css('display', 'none');
   }
 });
 
