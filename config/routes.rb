350f9e27043369a515c4f0c6d5683aa0dd077b45
DAVID MOORE <khundawg@gmail.com>
2016-03-23 15:01:44 -0400

    Update routes and add image files
    This commit adds in a update to the thank you route as well as introduce
    a few image assets that have been in limbo for some time.

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
   resources :experiences, only: [:index]
   resource :promos, only: [:show]
   resources :licensee_applications, only: [:new, :create]
-  get 'thank_you', to: 'licensee_applications', as: 'thank_you'
+  get 'thank_you', to: 'licensee_applications#thank_you', as: 'thank_you'
   resources :orders, only: [:new, :create, :show] do
     member do
       get 'cancel'
