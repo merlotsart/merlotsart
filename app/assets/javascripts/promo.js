9bf704e596e0ef981e2dce43b71f3aa3e0b6ef16
DAVID MOORE <khundawg@gmail.com>
2016-07-17 11:24:38 -0400

    Update promo JS

--- app/assets/javascripts/promo.js
+++ app/assets/javascripts/promo.js
@@ -1,19 +1,24 @@
 $( document ).ready(function() {
   $('form').on('change', '.promo-code', function() {
     var promoCode = $(this).val();
+    var subtotal = $('#subtotal').val;
     var request = $.ajax({
       url: '/promos',
       method: 'GET',
-      data: {promoCode: promoCode }
+      data: {promoCode: promoCode, subtotal: subtotal }
     })
     request.success(function(response) {
       if (response.valid) {
+
+        // NEEDS REFACTOR! No order math in javascript!
         var oldTotalFind = $('.total-price-summary').html();
         var oldTotal = oldTotalFind
         $('.total-price-new').html(accounting.formatMoney(parseInt(oldTotal) - (parseInt(oldTotal) * (parseInt(response.discount)/100))));
         $('.total-price-row').html(accounting.formatMoney(parseInt(oldTotal) - (parseInt(oldTotal) * (parseInt(response.discount)/100))));
         $('.promo-result').html("<label class='promo-success'>Promo Code Applied</label>");
-        $('.promo-result-row').html("<th>Promo Code</th><td>" + response.discount + "%</td>");
+        $('.promo-result-row').html("<th>Promo Code</th><td>" + response.discount + "</td>");
+        // END NEEDS REFACTOR! No order math in javascript!
+
       } else {
         $('.promo-result').html("<label class='promo-fail'>Promo Code Not Valid</label>");
       }
