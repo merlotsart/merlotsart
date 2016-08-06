fad8f6d5ee2531b7d403a262d33affcf56a08843
DAVID MOORE <khundawg@gmail.com>
2016-08-06 11:16:58 -0400

    Remove comments from orders and update method
    Preserving the old code comments in the previous commit. This one pulls
    them out.
    
    Adding the total parameter to persist_order method to keep orders
    working.

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -17,60 +17,20 @@ class OrdersController < ApplicationController
     subtotal      = params[:order][:total].to_i
     total         = CheckPromo.new(discount_code, subtotal).process_discount
 
-    # old code - pending delete
-    # if promo.valid?
-    #   discount = promo.discount
-    # else
-    #   discount = 0
-    # end
-
-    # if discount > 0
-    #   @price = (total - (total * (discount.to_f/100))).round(2)
-    # else
-    #   @price = total
-    # end
-
     if total > 0
-      # nonce     = params[:payment_method_nonce]
-      # result    = Braintree::Transaction.sale(
-      #   amount: total,
-      #   payment_method_nonce: nonce,
-      #     options: {
-      #       submit_for_settlement: true
-      #     }
-      #   )
 
       nonce  = params[:payment_method_nonce]
       result = ProcessBilling.process(nonce, total)
 
-      # if result.transaction != nil
-      #   @order.save
-      #   @order.update_attributes(public_event_id: @event.id, total: total, payment_id: result.transaction.id)
-      #   @order.order_create
-      #   UserMailer.confirmation(@order).deliver_now
-      #   redirect_to order_path(@order)
-      # else
-      #   flash[:notice] = "Sorry the card information was not correct, please try again."
-      #   render :new
-      # end
-
       if result.success?
-        persist_order(result.transaction.id)
+        persist_order(total, result.transaction.id)
       else
         flash[:notice] = "Sorry the card information was not correct, please try again."
         render :new
       end
 
-    # else
-    #   @order.save
-    #   UserMailer.confirmation(@order).deliver_now
-    #   @order.update_attributes(public_event_id: @event.id, total: total)
-    #   @order.order_create
-    #   redirect_to order_path(@order)
-    # end
-
     else
-      persist_order
+      persist_order(total)
     end
   end
 
@@ -91,7 +51,7 @@ class OrdersController < ApplicationController
     params.require(:order).permit(:name, :phone, :email, :public_event_id, :discount_code, :groupon_code, :quantity, :total, attendees_attributes: [:id, :name, :phone, :email, :discount_code])
   end
 
-  def persist_order(transaction_id = nil)
+  def persist_order(total, transaction_id = nil)
     @order.save
     @order.update_attributes(public_event_id: @event.id, total: total, payment_id: transaction_id)
     @order.order_create
