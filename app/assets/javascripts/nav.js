b605a6d9ccffdb17d75c5c71cbf9b74a093c6628
DAVID MOORE <khundawg@gmail.com>
2016-10-29 15:18:53 -0400

    Remove nav change on scroll
    Reverting to a more simple fixed nav

--- app/assets/javascripts/nav.js
+++ app/assets/javascripts/nav.js
@@ -1,5 +1,5 @@
 $(window).scroll(function(){
-  if ($(".navbar").offset().top > 20) {
+  if ($(".navbar-main").offset().top > 20) {
     $(".navbar-fixed-top").addClass("top-nav-scroll");
     $(".navbar-fixed-top").removeClass("top-nav-scroll-reverse");
     $(".merlots-logo-white").css('display', 'none');
@@ -11,16 +11,16 @@ $(window).scroll(function(){
     $(".merlots-logo-color").css('display', 'none');
   }
 
-  if ($(".navbar-landing").offset().top > 20) {
-    $(".navbar-fixed-top").addClass("top-nav-scroll-landing");
-    $(".navbar-fixed-top").removeClass("top-nav-scroll-reverse-landing");
-    $(".merlots-logo-white-landing").css('display', 'none');
-    $(".merlots-logo-color-landing").css('display', 'block');
-  } else {
-    $(".navbar-fixed-top").removeClass("top-nav-scroll-landing");
-    $(".navbar-fixed-top").addClass("top-nav-scroll-reverse-landing");
-    $(".merlots-logo-white-landing").css('display', 'block');
-    $(".merlots-logo-color-landing").css('display', 'none');
-  }
+  // if ($(".navbar-landing").offset().top > 20) {
+  //   $(".navbar-fixed-top").addClass("top-nav-scroll-landing");
+  //   $(".navbar-fixed-top").removeClass("top-nav-scroll-reverse-landing");
+  //   $(".merlots-logo-white-landing").css('display', 'none');
+  //   $(".merlots-logo-color-landing").css('display', 'block');
+  // } else {
+  //   $(".navbar-fixed-top").removeClass("top-nav-scroll-landing");
+  //   $(".navbar-fixed-top").addClass("top-nav-scroll-reverse-landing");
+  //   $(".merlots-logo-white-landing").css('display', 'block');
+  //   $(".merlots-logo-color-landing").css('display', 'none');
+  // }
 });
 
