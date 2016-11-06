48e188b7f9c8dce2f793b4334d593df62b3ce046
DAVID MOORE <khundawg@gmail.com>
2016-11-06 11:43:53 -0500

    Landing page tweaks
    Final tweaks in preparation for the launch:
    
    1. Update various copy
    2. Add form error styling
    3. Update welcome page navigation to link to new custom wall art page
    4. Update mobile layout of the "How it works" and "Pricing" sections of
       landing page.

--- app/models/project_request.rb
+++ app/models/project_request.rb
@@ -1,21 +1,11 @@
 class ProjectRequest < ActiveRecord::Base
 
-  RECTANGLE_CANVAS_SIZES = ["8\" x 10\"",
-                            "9\" x 12\"",
-                            "11\" x 14\"",
-                            "12\" x 16\"",
-                            "18\" x 24\"",
-                            "20\" x 24\"",
-                            "24\" x 36\"",
+  RECTANGLE_CANVAS_SIZES = ["11\" x 14\"",
+                            "16\" x 20\"",
                             "30\" x 40\"",
-                            "36\" x 48\"",
-                            "40\" x 60\""]
+                            "Custom"]
 
-  SQUARE_CANVAS_SIZES = [ "10\" x 10\"",
-                          "12\" x 12\"",
-                          "20\" x 20\"",
-                          "30\" x 30\"",
-                          "40\" x 40\""]
+  SQUARE_CANVAS_SIZES = [] # Presently unused but available for additional sizes
 
   SIZE_OPTIONS = { "Rectangle Sizes" => RECTANGLE_CANVAS_SIZES,
                    "Square Sizes" => SQUARE_CANVAS_SIZES }
