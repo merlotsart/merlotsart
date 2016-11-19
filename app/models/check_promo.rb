50f8ef738f3a645ab86760f014021fd5e3ab5a2a
DAVID MOORE <khundawg@gmail.com>
2016-11-19 11:43:34 -0500

    Update address in landing footer

--- app/models/check_promo.rb
+++ app/models/check_promo.rb
@@ -31,16 +31,6 @@ class CheckPromo
     end
   end
 
-  # This method is not 100% accurate to the concept of "percentage off". The
-  # problem is with the coversion of an integer representation of what would be
-  # a repleating percetage. Take a 33% discount for example:
-  #
-  #   33.to_f / 100  #=> .33
-  #   10 * .33       #=> 3.30
-  #   10 - 3.30      #=> 6.70, Not the 6.67 that would be expected.
-  #
-  # To correct this issue, we would need to convert to storing 3-place floats in
-  # the db as the percentage_discount.
   def calculate_percentage_discount
     @percentage = (@subtotal - (@subtotal * (@promo.discount_percentage.to_f/100))).round(2)
   end
